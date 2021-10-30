7ea98ddf39a8ec90eaf16ecf20a54482bc72e8b8
eduarsc <esebastiancprog@gmail.com>
2021-10-30 09:44:40 -0300

    regex- excercise 1 working

--- regex.py
+++ regex.py
@@ -1,1 +1,17 @@
+import re
 
+
+def aircraft_id_code_validator(id_code):
+    """ return True if the id_code of aircraft is rigth 
+        en other case false
+    
+
+    Args:
+        id_code (str): aricraft id code
+
+    Returns:
+        boolean: True is the id code follow the 
+        standart rules in other case False
+    """    
+    pattern = re.compile(r'''L[QV]-[A-Z]+\d*''')
+    return re.match(pattern, id_code) != None
