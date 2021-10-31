8e047e860d7c8bff0f3762ed9205e8d36355d750
LucasGiambelluca <lucasdavigiambelluca@gmail.com>
2021-10-31 04:12:28 -0300

    inicio de colections.py

--- colections.py
+++ colections.py
@@ -5,3 +5,37 @@
 
 
 
+
+
+
+
+
+
+#point Nº2..
+
+def List(list,num):
+    if num==0:
+        return list
+    else:
+        list.append(num)
+        num=num-1
+        return   List(list,num)
+
+num=int(input("enter a number: "))
+lista=[]
+new_list=List(lista,num)
+
+#map function..
+
+Map=list(map(lambda num: 4-((-1)**num)/(2*num)+1,new_list))
+
+print(Map)
+#function filter...
+
+Filtrado=list(filter(lambda numero: numero <= 3.14,Map))
+
+print(Filtrado)
+
+
+
+
