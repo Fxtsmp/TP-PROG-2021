118ead9eb240aa4ccbf8742ae4022c6100b95805
eduarsc <esebastiancprog@gmail.com>
2021-11-01 15:44:20 -0300

    exchange._data.py working with xml - added test in main

--- exchange_data.py
+++ exchange_data.py
@@ -1,1 +1,52 @@
+import xml.etree.cElementTree as et
+from utils import clear_screen
 
+
+def xml_list_availables_stations(root):
+    # listar estaciones disponibles
+    if root == None:
+        print("no information available - problem with read information from file")
+        return []
+    print("Listing availables stations")
+    list_of_stations = []
+    for station in root.findall("station"):
+        list_of_stations.append(station)
+        print(station.get("name"))
+    return list_of_stations
+
+
+def xml_showing_station_information(station, station_name):
+    print(
+        f'''Quantity of sensors in station {station.get("name").capitalize()}: {station.find("number_of_sensors").text}''')
+    for sensor in station[2:]:
+        if sensor.get("active") == "True":
+            print(
+                f'''{sensor.tag.capitalize()}\t:\t{sensor[0].text}{sensor[1].text}''')
+
+
+def xml_select_station(list_of_stations, position):
+    if len(list_of_stations) < position or list_of_stations == []:
+        return None
+    else:
+        return list_of_stations[position]
+
+def xml_get_data_from_file(file):
+    tree_ff = et.parse('./' + file)
+    root_ff = tree_ff.getroot()
+    return et.fromstring(et.tostring(root_ff, encoding='utf8').decode('utf8'))    
+
+
+'''
+    Algoritmo:
+    Listo todas las estaciones disponibles(nombres)
+    Pido nombre de estacion
+    Si el nombre esta en la lista:
+        mostrar(
+            Cantidad de sensores de la estación nombre estacion: cantidad de sensores
+            muestro todos los sensores disponibles, sensores que active="true" 
+            Temperatura: medida unidad
+            Humedad: medida unidad
+            Velocidad Viento: medida unidad
+        )
+    Sino 
+'''
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils import *
 from recursion import *
 from regex import *
 from colections import *
+from exchange_data import *
 
 def main():
     clear_screen()
@@ -17,16 +18,37 @@ def main():
     print(are_equals(int1, int2))
     print(integer_division_by_sustract(
         int(input("dividend ")), int(input("divisor ")))) """
-    print('''number_below_1900("1901")''',number_below_1900("1901"))
-    print('''number_below_1900("1899")''',number_below_1900("1899"))
-    print('''number_below_1900("1")''',number_below_1900("1"))
-    print('''number_below_1900("2000")''',number_below_1900("2000"))
-    print(f'''aircraft_id_code_validator("LQ-AWQ")\t=\t{"valida" if aircraft_id_code_validator("LQ-AWQ") else "invalida"}''')
-    print(f'''aircraft_id_code_validator("LV-123")\t=\t{"valida" if aircraft_id_code_validator("LV-123") else "invalida"}''')
-    print(f'''aircraft_id_code_validator("LV-XS123")\t=\t{"valida" if aircraft_id_code_validator("LV-XS123") else "invalida"}''')
-    print(f'''aircraft_id_code_validator("LV-S123")\t=\t{"valida" if aircraft_id_code_validator("LV-S123") else "invalida"}''')
-    print(f'''aircraft_id_code_validator("LV-XS")\t=\t{"valida" if aircraft_id_code_validator("LV-XS") else "invalida"}''')
+    print('''number_below_1900("1901")''', number_below_1900("1901"))
+    print('''number_below_1900("1899")''', number_below_1900("1899"))
+    print('''number_below_1900("1")''', number_below_1900("1"))
+    print('''number_below_1900("2000")''', number_below_1900("2000"))
+    print(
+        f'''aircraft_id_code_validator("LQ-AWQ")\t=\t{"valida" if aircraft_id_code_validator("LQ-AWQ") else "invalida"}''')
+    print(
+        f'''aircraft_id_code_validator("LV-123")\t=\t{"valida" if aircraft_id_code_validator("LV-123") else "invalida"}''')
+    print(
+        f'''aircraft_id_code_validator("LV-XS123")\t=\t{"valida" if aircraft_id_code_validator("LV-XS123") else "invalida"}''')
+    print(
+        f'''aircraft_id_code_validator("LV-S123")\t=\t{"valida" if aircraft_id_code_validator("LV-S123") else "invalida"}''')
+    print(
+        f'''aircraft_id_code_validator("LV-XS")\t=\t{"valida" if aircraft_id_code_validator("LV-XS") else "invalida"}''')
     print(calculate_pi(100))
+    
+    input("press enter to show xml exchange information")    
+    
+    root = xml_get_data_from_file("model.xml")
+    st = ""
+    l = []
+    while st.lower() != "x":
+        l = xml_list_availables_stations(root)
+        # pido nombre de la estacion
+        st = input("station to show information : ")
+        if st.lower() == "x":
+            break
+        xml_showing_station_information(xml_select_station(l, 2), st)
+        st = input(
+            "press enter to request new station information or [x] to e[x]it: ")
+        clear_screen()
 
 
 if __name__ == "__main__":
